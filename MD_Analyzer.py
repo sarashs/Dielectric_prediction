4d64b82588b36d3616a9591f63d7093acb458bc1
sarashs <arash.sheikh65@gmail.com>
2021-01-13 19:11:04 -0800

    More modifications and bug fixes.

--- MD_Analyzer.py
+++ MD_Analyzer.py
@@ -4,22 +4,26 @@ Created on Sun Aug 23 11:53:44 2020
 
 @author: sarashs
 """
-from random import randint
-#from matplotlib import pyplot as plot
+from random import randint, choice
+from matplotlib import pyplot as plot
 import time
-#import numpy as np
+import numpy as np
+
+#params = {'axes.labelsize': 36}#,'axes.titlesize':24, 'font.size': 24, 'legend.fontsize': 24, 'xtick.labelsize': 24, 'ytick.labelsize': 24}
+#plot.rcParams.update(params)
 
 Kb = 1.380649e-23
 ε0 = 8.8541878128e-12
 e_charge = 1.602176634e-19
 Angstrom = 1e-10
-coef = e_charge**2 / (ε0 * Kb * Angstrom)
+coef = e_charge**2 / (Angstrom)
 
 class MD_Analyzer(object):
     """This is a python class for analyzing LAMMPS outputs
     :attribute simulation_ID: string
+    ...
     """
-    def __init__(self, Trajectory_file_name, simulation_ID, columns=['ID', 'TYPE', 'CHARGE', 'X', 'Y', 'Z', 'Vx', 'Vy', 'Vz']):
+    def __init__(self, Trajectory_file_name, simulation_ID = 0, columns=['ID', 'TYPE', 'CHARGE', 'X', 'Y', 'Z', 'Vx', 'Vy', 'Vz']):
         self.number_of_atoms = 0
         self.place_holder = {'Dimensions' : 0, 'Masses' : 0, 'Atoms' : 0}
         self.data = {'Dimensions' : [0, 0, 0], 'Types' : {1 : {'mass' : 0, 'count' : 0, 'IDs' : []}}, 'data' : {1 : {'TYPE' : 0, 'CHARGE' : 0, 'X' : 0, 'Y' : 0, 'Z' : 0}}} 
@@ -28,9 +32,11 @@ class MD_Analyzer(object):
         self.magnetic_moment = 0 #total magnetic moment of the device
         self.Trajectory_file_name = Trajectory_file_name
         self.read_data(time_step = -1)
-        self.simulation_ID = simulation_ID
+        self.simulation_ID = str(simulation_ID)
         self.updated_lines = self.lines
         self.time_step = 0
+        self.neighbors = {}
+        self.neighbor_distance = 0
     def read_data(self, time_step = -1):
         # By default the last timestep will be read
         if self.Trajectory_file_name.endswith('.lammpstrj'):
@@ -64,9 +70,9 @@ class MD_Analyzer(object):
                 self.data['data'][int(temp[self.columns['ID']])] = {}
                 self.data['data'][int(temp[self.columns['ID']])]['TYPE'] = int(temp[self.columns['TYPE']]) 
                 self.data['data'][int(temp[self.columns['ID']])]['CHARGE'] = float(temp[self.columns['CHARGE']])
-                self.data['data'][int(temp[self.columns['ID']])]['X'] = float(temp[self.columns['X']])
-                self.data['data'][int(temp[self.columns['ID']])]['Y'] = float(temp[self.columns['Y']])
-                self.data['data'][int(temp[self.columns['ID']])]['Z'] = float(temp[self.columns['Z']])
+                self.data['data'][int(temp[self.columns['ID']])]['X'] = float(temp[self.columns['X']]) - float(x_dim[0])
+                self.data['data'][int(temp[self.columns['ID']])]['Y'] = float(temp[self.columns['Y']]) - float(y_dim[0])
+                self.data['data'][int(temp[self.columns['ID']])]['Z'] = float(temp[self.columns['Z']]) - float(z_dim[0])
             self.file.close()
                 
         if self.Trajectory_file_name.endswith('.data'):
@@ -114,47 +120,96 @@ class MD_Analyzer(object):
                             self.data['data'][ID] = {'TYPE' : int(temp[type_index]), 'CHARGE' : float(temp[charge_index]), 'X' : float(temp[x_index]), 'Y' : float(temp[y_index]), 'Z' : float(temp[z_index])}
 
             self.file.close()
-    def periodic_condition(self, inputs):
+    def recenter(self, inputs):
         output = inputs
         for i,j in enumerate(output):
-            if abs(output[i]) >= self.data['Dimensions'][i] / 2:
-                if output[i] >= 0:
-                    output[i] -= self.data['Dimensions'][i] / 2
-                else:
-                    output[i] += self.data['Dimensions'][i] / 2
+            output[i] -= self.data['Dimensions'][i]/2
         return output
-    def Dipole_moment(self, time_step, radius):
+    def periodic_distance(self, a, b):
+        pass
+    def sphere_percentage(self,radius):
+        ## molar percent calculator
+        num_zr = 0
+        num_si = 0
+        ##
+        for i in range(1, self.number_of_atoms + 1):
+            if np.linalg.norm(self.recenter([self.data['data'][i]['X'], self.data['data'][i]['Y'], self.data['data'][i]['Z']])) < radius:
+                ## molar percent calculator
+                if self.data['data'][i]['TYPE'] == 3:
+                    num_zr += 1
+                elif self.data['data'][i]['TYPE'] == 2:
+                    num_si += 1
+                ##
+        return num_zr/(num_zr + num_si)
+    def sphere_set(self,radius):
+        ID_list = []
+        for i in range(1, self.number_of_atoms + 1):
+            if np.linalg.norm(self.recenter([self.data['data'][i]['X'], self.data['data'][i]['Y'], self.data['data'][i]['Z']])) < radius:
+                ID_list.append(i)
+        return ID_list
+    def cube_set(self,from_the_edge):
+        pass
+    def cube_percentage(self,from_the_edge):
+        """Returns the set of atoms which are at least from_the_edge distance from the edge"""
+        ## molar percent calculator
+        num_zr = 0
+        num_si = 0
+        ##
+        for i in range(1, self.number_of_atoms + 1):
+            if (self.data['data'][i]['X'] - from_the_edge > 0) and (self.data['data'][i]['X'] + from_the_edge < self.data['Dimensions'][0]):
+                if (self.data['data'][i]['Y'] - from_the_edge > 0) and (self.data['data'][i]['Y'] + from_the_edge < self.data['Dimensions'][1]):
+                    if (self.data['data'][i]['Z'] - from_the_edge > 0) and (self.data['data'][i]['Z'] + from_the_edge < self.data['Dimensions'][2]):
+                        ## molar percent calculator
+                        if self.data['data'][i]['TYPE'] == 3:
+                            num_zr += 1
+                        elif self.data['data'][i]['TYPE'] == 2:
+                            num_si += 1
+                        ##
+        return num_zr/(num_zr + num_si)
+    def Dipole_moment(self, time_step, ID_list):
         MF = 0
         temp = [0, 0, 0]
         self.read_data(time_step)
-        for i in range(1, self.number_of_atoms + 1):
-            atom = self.periodic_condition([self.data['data'][i]['X'], self.data['data'][i]['Y'], self.data['data'][i]['Z']])
-            if np.linalg.norm(atom) < radius:
-                temp = [k*self.data['data'][i]['CHARGE']+temp[j] for j,k in enumerate(atom)]
-        MF = np.linalg.norm(temp)
-        return MF
-    def Dipole_moment_fluctuation(self, init_timestep, span_timestep, final_timestep, T):
+        for i in ID_list: #range(1, self.number_of_atoms + 1):
+            atom = self.recenter([self.data['data'][i]['X'], self.data['data'][i]['Y'], self.data['data'][i]['Z']]) 
+            #if self.data['data'][i]['ID'] in ID_list:
+            temp = [k*self.data['data'][i]['CHARGE']+temp[j] for j,k in enumerate(atom)]
+        #MF = np.linalg.norm(temp)#/number_of_molecules_in_sphere
+        return np.array(temp)
+    def Dipole_moment_fluctuation(self, init_timestep, span_timestep, final_timestep, T, radius, shape = 'sphere'):
+        if 'cube' in shape:
+            ID_list = self.cube_set(radius)
+        else:
+            ID_list = self.sphere_set(radius)
         start =  time.time()
         current_timestep = init_timestep
-        DMF = self.Dipole_moment(current_timestep, 10)
-        DMF2 = DMF ** 2
+        DMF = self.Dipole_moment(current_timestep, ID_list)
+        #DMF = MF
+        DMF2 = DMF*DMF
         DMF_list = []
+        MF_list = []
         num = 1
-        c = coef / (3 * T)
+        c = coef/(ε0 * Kb*T)
         while current_timestep < final_timestep:
+            #print(current_timestep)
             num += 1
             current_timestep += span_timestep
-            MF = self.Dipole_moment(current_timestep, 10)
+            MF = self.Dipole_moment(current_timestep, ID_list)
             DMF += MF
-            DMF2 += MF ** 2
-            beta = c * (DMF2 / num - (DMF / num) ** 2)  / (np.pi * 4 * 1000)
-            DMF_list.append(((1 + 3 * beta) + ((1 + 3 * beta)**2 + 8)**0.5)/4)
+            DMF2 += MF*MF
+            beta = c * np.sum(DMF2 / (num) - (DMF*DMF )/ (num**2))  
+            if 'cube' in shape:
+                beta = beta /(3 * (self.data['Dimensions'][0] - 2 * radius) * (self.data['Dimensions'][1] - 2 * radius) * (self.data['Dimensions'][2] - 2 * radius))
+            else:
+                beta = beta /(4 * np.pi * radius**3)
+            DMF_list.append(beta + 1)#((1 + 3 * beta) + ((1 + 3 * beta)**2 + 8)**0.5)/4)
+            MF_list.append(sum(MF))
         end = time.time()
         print(f'it took {end - start} seconds.')
-        return DMF_list
+        return MF_list#DMF_list
     def save_as_lammps_data(self, time_step = -1):
         # By default the last timestep will be read
-        if time_step != 1:
+        if time_step != -1:
             self.read_data(time_step)
         file = open(self.LAMMPS_Data_file.replace('.data','') + self.simulation_ID + '.data', 'w')
         file.write('# System description #######################\n' + '#\n\n' + str(self.number_of_atoms) + ' atoms\n' + str(len(list(self.data['Types'].keys()))) + ' atom types\n')
@@ -171,12 +226,24 @@ class MD_Analyzer(object):
             file.write(str(i).ljust(6) + str(self.data['data'][i]['TYPE']).ljust(4) + str(self.data['data'][i]['CHARGE']).ljust(12) + str(self.data['data'][i]['X']).ljust(12) + str(self.data['data'][i]['Y']).ljust(12) + str(self.data['data'][i]['Z']) + '\n')
         file.close()
     def replace_atoms(self, type_final, ID):
+        """
+        Changes the type of an atom
+        """
         current_type = self.data['data'][ID]['TYPE']
         self.data['data'][ID]['TYPE'] = type_final
         self.data['Types'][current_type]['count'] -= 1
         self.data['Types'][current_type]['IDs'].remove(ID)
         self.data['Types'][type_final]['count'] += 1
         self.data['Types'][type_final]['IDs'].append(ID)
+    def replace_atom_number(self, num_atm_replace, type_init, type_final):
+        """Replace num_atm_replace atoms of type_init to type_final. It is done randomly"""
+        assert (type_init in self.data['Types']), f"{type_init} is not an available data type." 
+        if(type_final not in self.data['Types']):
+            self.data['Types'][type_final] = {'mass' : 0, 'count' : 0, 'IDs' : []}
+            print(f"Critical Warning: Type {type_final} was not available and was added to the types. Other associated features such as mass must be added manually.")
+        for i in range(num_atm_replace):
+             ID = choice(self.data['Types'][type_init]['IDs'])
+             self.replace_atoms(type_final, ID)
     def create_lammps_input(self, Input_forcefield, type_of_simulation = 'anneal', **kwargs):
         """
         This function creates the lammps input file
@@ -279,8 +346,8 @@ class MD_Analyzer(object):
             s.write('reset_timestep	0\n')
             s.write('timestep 0.5\n')
             s.write('restart 500000 ' + self.LAMMPS_Data_file.replace('.data','') + self.simulation_ID + '.restart\n')
-            s.write('fix MD6 all nvt temp 300 300 50.0\n')
-            s.write('dump DUMP4 all custom 20000 ' + 'fluctuate_' + self.LAMMPS_Data_file.replace('.data','') + self.simulation_ID + '.lammpstrj'+' id type x y z q #this size \n')
+            s.write('fix MD6 all nvt temp 300 300 20\n')
+            s.write('dump DUMP4 all custom 20000 ' + 'fluctuate_' + self.LAMMPS_Data_file.replace('.data','') + self.simulation_ID + '.lammpstrj'+' id type x y z q #this size \n')        
             s.write('thermo_style custom step etotal ke pe temp press pxx pyy pzz \n')
             s.write(f'thermo {fluctuation_thermo_duration}\n')
             s.write(f'run {fluctuation_duration}\n')
@@ -311,24 +378,11 @@ class MD_Analyzer(object):
             fluctuation_thermo_duration = 10000
         s.write('timestep 0.5\n')
         s.write('restart 500000 ' + self.Trajectory_file_name.replace('.lammpstrj','') + '.restart\n')
-        s.write('fix MD6 all nvt temp 300 300 50.0\n')
-        s.write('dump DUMP4 all custom 20000 ' + 'fluctuate_' + self.Trajectory_file_name +' id type x y z q #this size \n')
+        s.write('fix MD6 all nvt temp 300 300 20.0\n')
+        s.write('dump DUMP4 all custom 10000 ' + 'fluctuate_' + self.Trajectory_file_name +' id type x y z q #this size \n')
         s.write('thermo_style custom step etotal ke pe temp press pxx pyy pzz \n')
         s.write(f'thermo {fluctuation_thermo_duration}\n')
         s.write(f'run {fluctuation_duration}\n')
         s.write('unfix MD6\n')
         s.write('undump DUMP4\n')
         s.close()
-    def consistent_plot(self,X, Y, labels, formats, alphas, xlabel, ylabel, title, save_name):
-        """The arguments are all lists!"""
-        for i, j in enumerate(labels):
-            plot.plot(X[i], Y[i], formats[i], alpha = alphas[i], markersize=10.0,linewidth=2.0,label = labels[i])
-        plot.xlabel(xlabe, fontsize = 14)
-        plot.ylabel(ylabe, fontsize = 14)
-        plot.xticks(fontsize = 14)
-        plot.yticks(fontsize = 14)
-        plot.title(title, fontsize=14)
-        plot.legend(fontsize=14)
-        plot.grid()
-        plot.savefig(save_name + '.png')
-        plot.show()
--- main_Niagra.py
+++ main_Niagra.py
@@ -9,11 +9,11 @@ from MD_Analyzer import MD_Analyzer as MDA
 import os
 from random import choice
     
-def create_bash_files():
+def create_bash_files(num_nodes = 2):
     for file_name in os.listdir("./"):
         if file_name.endswith(".in"):
-            f = open(file_name[:-3] + ".sh", 'w')
-            f.write("#!/bin/bash\n" + "#SBATCH --account=def-ivanov\n" + "#SBATCH --time=1-00:00\n" + "#SBATCH --nodes=2\n" + "#SBATCH --ntasks=160\n\n" + "module load intel/2019u3  intelmpi/2019u3  lammps/29Mar2019\n")
+            f = open(file_name[:-3] + f"_node{num_nodes}.sh", 'w')
+            f.write("#!/bin/bash\n" + "#SBATCH --account=def-ivanov\n" + "#SBATCH --time=0-00:30\n" + f'#SBATCH --nodes={num_nodes}\n' + f'#SBATCH --ntasks={num_nodes*80}\n\n' + "module load intel/2019u3  intelmpi/2019u3  lammps/29Mar2019\n")
             f.write("\n" + "lmp_exec=lammps\n")
             f.write("lmp_input=" + "\"" + file_name + "\"\n")
             f.write("lmp_output=" + "\"" + file_name[:-3] + ".lammpslog\"\n")
@@ -24,27 +24,32 @@ def run_bash():
         if file_name.endswith(".sh"):
             os.system("sbatch " + file_name)
        
-#percentages = [2, 5, 7, 10, 12, 15, 17]
-#a = MDA('anneal_dielectricpercent_2_number_0.lammpstrj', '', columns=['ID', 'TYPE', 'X', 'Y', 'Z', 'CHARGE'])
-#a.data['Types'][3] = {'mass' : 91.224, 'count' : 0, 'IDs' : []}
-#for percent in percentages:
-#    for num in range(10):
-#        a.LAMMPS_Data_file = 'dielectric.data'
-#        a.simulation_ID = 'percent_{}_number_{}'.format(percent, num)
-#        while (a.data['Types'][3]['count']/a.data['Types'][2]['count']) < percent/100:
-#            a.replace_atoms(3, choice(a.data['Types'][2]['IDs']))
-#        a.save_as_lammps_data()
-#        a.create_lammps_input('ffield.reax')
+#Generate input files for different percentages
+#for i in [160, 400, 800, 1200, 1360]:
+#    a = MDA('Large_cell.data', columns=['ID', 'TYPE',  'X', 'Y', 'Z', 'CHARGE'])
+#    a.replace_atom_number(i, 2, 3)
+#    a.data['Types'][3]['mass'] = 91.224
+#    a.simulation_ID = str(int(i*100/8000))
+#    a.save_as_lammps_data()
+#    a.create_lammps_input('ffield.reax', type_of_simulation = 'anneal', equiliberation_duration = 100000, annealing_duration = 100000000)
 #create_bash_files()
 #run_bash()
+########################
+# find the fastest combination
+a = MDA('Large_cell.data', columns=['ID', 'TYPE',  'X', 'Y', 'Z', 'CHARGE'])
+a.save_as_lammps_data()
+a.create_lammps_input('ffield.reax', type_of_simulation = 'anneal', equiliberation_duration = 100000, annealing_duration = 500000)
+for num_nodes in [1,2,3,4]:
+    create_bash_files()
+run_bash()            
 #######################
-for file_name in os.listdir("./"):
-    if file_name.endswith('lammpstrj') and file_name.startswith('anneal'):            
-        print('here')
-        a = MDA(file_name, '', columns=['ID', 'TYPE', 'X', 'Y', 'Z', 'CHARGE']) 
-        a.restart_fluctuate(2500000)
-create_bash_files()
-run_bash()
+#for file_name in os.listdir("./"):
+#    if file_name.endswith('lammpstrj') and file_name.startswith('anneal'):            
+#        print('here')
+#        a = MDA(file_name, '', columns=['ID', 'TYPE', 'X', 'Y', 'Z', 'CHARGE']) 
+#        a.restart_fluctuate(2500000)
+#create_bash_files()
+#run_bash()
 #######################
 #for file_name in os.listdir("./"):
 #    if file_name.endswith(".lammpstrj"):            
